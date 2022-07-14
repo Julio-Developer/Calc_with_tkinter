1db9d258405aa24bfadd77e24a9488642c01c6ec
Júlio Gabriel <julioggoliveira77@gmail.com>
2022-07-14 07:06:43 -0300

    Commit Inicial

---
+++ Calc.py
@@ -0,0 +1,15 @@
+# Import libraries
+import tkinter
+
+class Calc: 
+
+    def __init__(self):
+        
+        self.window = tkinter()
+        self.window.title('Calc')
+        self.window.resized
+
+
+        self.window.mainloop()
+
+Calc()
